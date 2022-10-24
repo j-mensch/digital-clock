6ce19fae576d3b47c6f55bd961bc9d626f360961
Justin Mensch <justin.p.mensch@gmail.com>
2022-10-24 15:44:02 -0700

    resolved issue with clock not continuously running

--- main.py
+++ main.py
@@ -6,11 +6,11 @@
 from tkinter import Label, Tk
 import time
 
-#construct application window with title, set dimensions, & make it non-resizable
+#construct application window with title, set dimensions, & make it resizable
 app_window = Tk()
 app_window.title("Digital Clock")
 app_window.geometry('500x200')
-app_window.resizable(0,0)
+app_window.resizable(1,1)
 
 #define the text font style, colors, and set border width
 text_font = ("Boulder", 68, 'bold')
@@ -20,12 +20,13 @@ border_width = 25
 
 #define the label variable and pass it the params defined above
 label = Label(app_window, font = text_font, bg = background, fg = foreground, bd = border_width)
-label.grid(row = 1, column = 3)
+label.grid(row = 0, column = 1)
 
 #set the digital clock to display {hour/min/sec} 
 def digital_clock():
     time_live = time.strftime("%H:%M:%S")
     label.config(text=time_live)
+    label.after(200, digital_clock)
 
 #invoke/initiate the digital clock
 digital_clock()
